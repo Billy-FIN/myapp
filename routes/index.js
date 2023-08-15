1322af9214945be4929e7ff3a865e8f9214d24d5
Qiuyang Wang <qiuyangwang@brandeis.edu>
2023-08-15 21:46:37 +0800

    added about

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/test', function(req, res, next) {
+  res.render('test');
+});
+
 module.exports = router;
+
+
