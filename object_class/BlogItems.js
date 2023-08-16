9500f46138521eeba3f3a9e01ef689e6b1f942de
Qiuyang Wang <qiuyangwang@brandeis.edu>
2023-08-16 22:51:26 +0800

    initialed to develop back-end management for blogs

---
+++ object_class/BlogItems.js
@@ -0,0 +1,16 @@
+
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var BlogItemSchema = Schema({
+  title: String,
+  category: String,
+  date: Date,
+  display: Boolean,
+  userId: { type: ObjectId, ref: 'user' },
+  username: String
+});
+
+module.exports = mongoose.model('BlogItems', BlogItemSchema);
