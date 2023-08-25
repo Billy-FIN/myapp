acce2c72d4efb7ef11ba6f90c7e520aa3483eecf
Qiuyang Wang <qiuyangwang@brandeis.edu>
2023-08-25 15:43:07 +0800

    finished the filter of Notebook

--- routes/notebook.js
+++ routes/notebook.js
@@ -3,66 +3,47 @@ const router = express.Router();
 const NotesItem = require('../object_class/NotesItem');
 
 router.get('/notebook', async function (req, res, next) {
-  let sortBy
-  if(req.query.searchBy != undefined){
-    req.session.searchBy = req.query.searchBy
-  }
-  if(req.query.sortBy != undefined){
-    sortBy = req.query.sortBy
-  }
-  let searchBy = req.session.searchBy
+  items = []
+  const q = req.query.q
+  if (q == "null") {
+    items = await NotesItem.find({}).sort({ date: -1 })
+    req.session.searchBy = ""
+    res.render('notebook', { items, searchBy: '' });
+  } else {
+    let sortBy
+    if (req.query.searchBy != undefined) {
+      req.session.searchBy = req.query.searchBy
+    }
+    if (req.query.sortBy != undefined) {
+      sortBy = req.query.sortBy || undefined
+    }
+    let searchBy = req.session.searchBy
 
-  let items = await NotesItem.find({}).sort({ date: -1 })
-  if (typeof (searchBy) == 'string') {
-    items = await NotesItem.find({ $or: [{ title: { $regex: searchBy } }, { category: { $regex: searchBy } }, { tag: { $regex: searchBy } }, { details: { $regex: searchBy } }, { username: { $regex: searchBy } }] })
+    items =
+      await NotesItem.aggregate(
+        [
+          {
+            $match: {
+                     $or: [{ title: { $regex: searchBy } },
+                           { category: { $regex: searchBy } },
+                           { tag: { $regex: searchBy } },
+                           { details: { $regex: searchBy } },
+                           { username: { $regex: searchBy } }],
+                    }
+          },
+          {
+            $match: { category: { $regex: req.query.category ? req.query.category : '' },
+                      tag: { $regex: req.query.tag ? req.query.tag : '' },
+                      username: { $regex: req.query.username? req.query.username : '' }
+                    }
+          },
+          {
+            $sort: { [sortBy]: 1 }
+          }
+        ]
+      )
+    res.render('notebook', { items, searchBy });
   }
-
-  if(typeof (req.query.category) == 'string' || typeof (req.query.tag) == 'string' || typeof (req.query.username) == 'string'){
-    items = await NotesItem.find({ $and: [{ category: { $regex: req.query.category } }, { tag: { $regex: req.query.tag } }, { username: { $regex: req.query.username } }] })
-  }
-  switch (sortBy) {
-    case 'category':
-      items = items.sort((a, b) => a.category.localeCompare(b.category));
-      break;
-    case 'tag':
-      items = items.sort((a, b) => a.tag.localeCompare(b.tag));
-      break;
-    case 'username':
-      items = items.sort((a, b) => a.username.localeCompare(b.username));
-      break;
-    default:
-      break;
-  }
-  res.render('notebook', { items });
-
-
-
-  // if (typeof (sortBy) == 'string') {
-  //   switch (sortBy) {
-  //     case 'category':
-  //       items = await NotesItem.find({}).sort({ category: 1 })
-  //       break;
-  //     case 'tag':
-  //       items = await NotesItem.find({}).sort({ tag: 1 })
-  //       break;
-  //     case 'date':
-  //       items = await NotesItem.find({}).sort({ date: -1 })
-  //       break;
-  //     case 'username':
-  //       items = await NotesItem.find({}).sort({ username: 1 })
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // } else if (typeof (searchBy) == 'string') {
-  //   items = await NotesItem.find({ $or: [{ title: { $regex: searchBy } }, { category: { $regex: searchBy } }, { tag: { $regex: searchBy } }, { details: { $regex: searchBy } }, { username: { $regex: searchBy } }] })
-  // } else if (typeof (selectBy) == 'string') {
-  //   let conditions = selectBy.split('&')
-  //   items = await NotesItem.find({ $and: [{ category: { $regex: conditions[0] } }, { tag: { $regex: conditions[1] } }, { username: { $regex: conditions[2] } }] })
-  // } else {
-  //   items = await NotesItem.find({}).sort({ date: -1 })
-  // }
-  // res.render('notebook', { items });
 });
 
 router.post('/notebook',
