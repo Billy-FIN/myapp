5d1eb3dd1ca6b489d4f9f353e8d1ead588b757e2
Qiuyang Wang <qiuyangwang@brandeis.edu>
2023-09-05 22:20:58 -0400

    solved the security problem of explicitly showing mongodb_uri in this repo

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var compression = require('compression');
 // var helmet = require('helmet');
 
 // Connecting to a Mongo Database Server
-const mongodb_URI = 'mongodb://127.0.0.1:27017/pwdemo';
+const mongodb_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pwdemo';
 console.log('MONGODB_URI=', mongodb_URI);
 const mongoose = require('mongoose');
 mongoose.connect(mongodb_URI);
