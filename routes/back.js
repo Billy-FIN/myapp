f37760c41a6a9e8d06cb671d08698714970533d1
Qiuyang Wang <qiuyangwang@brandeis.edu>
2023-09-09 01:07:53 -0400

    started developing back management user interface

--- routes/back.js
+++ routes/back.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/back_management', function(req, res, next) {
-  res.render('back_management');
+  res.render('back_management', );
 });
 
 module.exports = router;
